c3abe7c0a8b975df83929cec56a3cb1d39cd5793
Kent Fredric <kentfredric@gmail.com>
2013-12-07 07:36:28 +1300

    First version, with test

--- lib/Dist/Zilla/Util/Git/Wrapper.pm
+++ lib/Dist/Zilla/Util/Git/Wrapper.pm
@@ -3,9 +3,52 @@ use warnings;
 
 package Dist::Zilla::Util::Git::Wrapper;
 
-# ABSTRACT:
+# ABSTRACT: Vivify a Git instance from Dzil
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Util::Git::Wrapper;
+
+    my $wrapper =  Dist::Zilla::Util::Git::Wrapper->new( zilla => $self->zilla );
+
+    $wrapper->log(); # etc.
+
+Yes, this does very little, but simply serves as an abstraction for getting a Git::Wrapper
+object from git, mostly, because everyone remembers C<< $self->zilla >>, but you have to RTFM
+and go source diving to know about C<< $self->zilla->root >>
+
+This is a simple straight forward way of doing what you want with the least mental overhead.
+
+Everything else is just proxy methods for C<< git >>
+
+This is a more IOC approach to my swathes of C<< Dist::Zilla::Role::Git::LocalRepository >> stuff,
+which got so complex with dependency management my head exploded.
+
+=cut
 
 use Moose;
+use MooseX::LazyRequire;
+use Git::Wrapper;
+
+our $AUTOLOAD;
+
+sub AUTOLOAD {
+    my ($self, @args ) = @_;
+    my $meth = $AUTOLOAD;
+    $meth =~ s/.+:://;
+    return if $meth eq 'DESTROY';
+    return $self->git->$meth( @args );
+}
+
+has zilla => ( isa => 'Object', is => 'ro', lazy_required => 1 );
+has git   => ( isa => 'Object', is => 'ro', lazy_build    => 1 );
+
+sub _build_git {
+    my ( $self, @args ) = @_;
+    return Git::Wrapper->new(  $self->zilla->root );
+}
+
+
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
