848c7bad204e6085fa4a512c4bdfb67f7394be3f
Kent Fredric <kentfredric@gmail.com>
2013-12-21 02:06:33 +1300

    Add UtilRole and test

---
+++ lib/Dist/Zilla/UtilRole/MaybeGit.pm
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use utf8;
+
+package Dist::Zilla::UtilRole::MaybeGit;
+
+# ABSTRACT: A role to make adding a ->git method easy, and low-complexity
+
+use Moose::Role;
+with 'Dist::Zilla::UtilRole::MaybeZilla';
+
+has 'git' => ( is => ro =>, isa => Object =>, lazy_build => 1 );
+
+sub _build_git {
+  my ($self) = @_;
+  require Dist::Zilla::Util::Git::Wrapper;
+  return Dist::Zilla::Util::Git::Wrapper->new( zilla => $self->zilla );
+}
+
+no Moose::Role;
+1;
+
